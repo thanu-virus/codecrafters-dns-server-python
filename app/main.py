83d4751dfe5641f78f855607604c56b5bbdd3a52
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-09-29 10:57:46 +0530

    [ commit for question section]

--- app/main.py
+++ app/main.py
@@ -6,13 +6,18 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    #
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #  AF_INET specifies that it's an IPv4 socket, and SOCK_DGRAM indicates it's a UDP socket.
+    udp_socket = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-            response = b"\x04\xd2\x80"+(b"\x00"*9)
+            rid=b"\x04\xd2"
+            rflags=b"\x80\x00"
+            qdcount=b"\x00\x01"
+            header =rid+rflags+qdcount+ (b"\x00"*6)
+            question = b"\x0ccodecarfters\x02io\x00\x00\x01\x00\x01"
+            response=header+question
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
