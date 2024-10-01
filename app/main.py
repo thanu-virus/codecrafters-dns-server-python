8552c3ddaa00a32ea663ded4abf04cdf8450ebd0
thanu-virus <130907493+thanu-virus@users.noreply.github.com>
2024-10-01 22:47:32 +0530

    [final commit for answer]

--- app/main.py
+++ app/main.py
@@ -1,27 +1,33 @@
 import socket
+
+
 def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Uncomment this block to pass the first stage
+    #  AF_INET specifies that it's an IPv4 socket, and SOCK_DGRAM indicates it's a UDP socket.
+    udp_socket = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-            rid = b"\x04\xd2"
-            rflags = b"\x80\x00"
-            qdcount = b"\x00\x01"
-            ancount = b"\x00\x01"
-            header = rid + rflags + qdcount + ancount + (b"\x00" * 4)
+            rid=b"\x04\xd2"
+            rflags=b"\x80\x00"
+            qdcount=b"\x00\x01"
+            ancount=qdcount
+            header =rid+rflags+qdcount+ancount+ (b"\x00"*6)
             question = b"\x0ccodecrafters\x02io\x00\x00\x01\x00\x01"
             ttl = b"\x00\x00\x00\x3c"
             data = b"\x08\x08\x08\x08"
             data_len = b"\x00\x04"
-            answer = (
-                b"\x0ccodecrafters\x02io\x00\x00\x01\x00\x01" + ttl + data_len + data
-            )
-            response = header + question + answer
+            answer=(b"\x0ccodecrafters\x02io\x00\x00\x01\x00\x01"+ttl+data_len+data)
+            response=header+question+answer
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
+
+
 if __name__ == "__main__":
     main()
